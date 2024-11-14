12d5da1113eeb36eb837ea69b89b3b95bddcbdf2
thevipsishere <104143418+thevipsishere@users.noreply.github.com>
2024-11-14 12:29:23 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here if needed
     },
-    baseUrl: 'http://localhost:3000' // Replace with the base URL for your application
+    baseUrl: 'http://localhost:3000' // or your specific base URL
   },
 });
