7b818cfd9ea75a522f6dbdbb1fe1c3f3205b96d7
thevipsishere <104143418+thevipsishere@users.noreply.github.com>
2024-11-15 13:13:14 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['html', 'text-summary'],
-  testEnvironment: 'node',
+  coverageReporters: ['text', 'lcov'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
 };
