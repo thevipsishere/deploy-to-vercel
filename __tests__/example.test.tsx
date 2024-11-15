f4adba2c356ea05deeac4aab2f954e33c53df277
thevipsishere <104143418+thevipsishere@users.noreply.github.com>
2024-11-15 13:15:18 +0530

    Delete __tests__ directory

--- __tests__/example.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-test("renders a sample component", () => {
-  const { getByText } = render(<div>Sample Component</div>);
-  expect(getByText("Sample Component")).toBeTruthy();
-});
