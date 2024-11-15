8742d4c4fb881044eca795ceee44d45315bdb3b1
thevipsishere <104143418+thevipsishere@users.noreply.github.com>
2024-11-15 12:54:55 +0530

    Update example.test.tsx

--- __tests__/example.test.tsx
+++ __tests__/example.test.tsx
@@ -1,7 +1,7 @@
-import { render } from "@testing-library/react";
 import React from "react";
+import { render } from "@testing-library/react";
 
-test("renders a test component", () => {
-  const { getByText } = render(<div>Test Component</div>);
-  expect(getByText("Test Component")).toBeTruthy();
+test("renders a sample component", () => {
+  const { getByText } = render(<div>Sample Component</div>);
+  expect(getByText("Sample Component")).toBeTruthy();
 });
