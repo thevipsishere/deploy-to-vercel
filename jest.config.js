f29478f64f1d7919db32a97f06f9c2a6f2f19ee9
thevipsishere <104143418+thevipsishere@users.noreply.github.com>
2024-11-15 12:57:01 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  testEnvironment: "jest-environment-jsdom", // Specify the jsdom test environment
+  testEnvironment: "jest-environment-jsdom", // Use jsdom for React tests
   transform: {
-    "^.+\\.tsx?$": "ts-jest" // Use ts-jest for TypeScript support
+    "^.+\\.tsx?$": "ts-jest", // Use ts-jest to transform TypeScript files
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"], // Recognize these file extensions
   collectCoverage: true, // Enable coverage report
-  coverageDirectory: "coverage", // Directory to store coverage output
+  coverageDirectory: "coverage", // Directory for coverage output
   coverageReporters: ["json", "lcov", "text", "clover"], // Formats for coverage output
 };
