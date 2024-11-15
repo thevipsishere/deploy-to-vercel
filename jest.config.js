7496155efa7b12e639422d7b2dee5bc3bc80e647
thevipsishere <104143418+thevipsishere@users.noreply.github.com>
2024-11-15 12:32:57 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  collectCoverage: true, // Enable coverage collection
+  coverageDirectory: "coverage", // Directory for coverage output
+  coverageReporters: ["json", "lcov", "text", "clover"], // Formats for the coverage report
+};
